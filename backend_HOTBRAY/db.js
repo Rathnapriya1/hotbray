c74c8c564685b816e5dda3e5856824565053e2fe
Rathnapriya1 <rathnapriya@dgstechlimited.com>
2025-11-06 11:42:10 +0530

    db.js

--- backend_HOTBRAY/db.js
+++ backend_HOTBRAY/db.js
@@ -5,12 +5,10 @@ dotenv.config();
 
 const { Pool } = pg;
 
+// Use only the connection string in production
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // required for Neon DB
 });
 
 export default pool;
